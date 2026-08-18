27b59c8217b696f087810de928e2157dd7f07f05
Elik <elikbelikshluk@gmail.com>
2026-08-18 15:56:24 +0300

    Add a third ball. When it is clicked, swap the colors and sizes of the first two balls

--- main.js
+++ main.js
@@ -15,3 +15,24 @@ function onBallClick(elBall, maxDiameter) {
 
     elBall.style.backgroundColor = getRandomColor()
 }
+
+function onThirdBallClick() {
+
+    const elBall1 = document.querySelector('.ball1')
+    const elBall2 = document.querySelector('.ball2')
+
+    const tempSize = elBall1.style.width
+    const tempColor = elBall1.style.backgroundColor
+    const tempText = elBall1.innerText
+
+    elBall1.innerText = elBall2.innerText
+    elBall2.innerText = tempText
+
+    elBall1.style.width = elBall2.style.width
+    elBall1.style.height = elBall2.style.height
+    elBall1.style.backgroundColor = elBall2.style.backgroundColor
+
+    elBall2.style.width = tempSize
+    elBall2.style.height = tempSize
+    elBall2.style.backgroundColor = tempColor
+}
